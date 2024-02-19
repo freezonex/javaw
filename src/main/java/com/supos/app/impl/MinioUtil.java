9b0d628da28b4a4617c43fa8c06728ff16d92df3
Wenhao <yuwenhao@freezonex.io>
2024-02-20 05:46:56 +0800

    minio管理页面优化

--- src/main/java/com/supos/app/impl/MinioUtil.java
+++ src/main/java/com/supos/app/impl/MinioUtil.java
@@ -41,6 +41,7 @@ public class MinioUtil {
 
     /**
      * 查看存储bucket是否存在
+     *
      * @return boolean
      */
     public Boolean bucketExists(String bucketName) {
@@ -56,34 +57,34 @@ public class MinioUtil {
 
     /**
      * 创建存储bucket
+     *
      * @return Boolean
      */
     public Boolean makeBucket(String bucketName) {
         try {
-            minioClient.makeBucket(MakeBucketArgs.builder()
-                    .bucket(bucketName)
-                    .build());
+            minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
         } catch (Exception e) {
             e.printStackTrace();
             return false;
         }
         return true;
     }
+
     /**
      * 删除存储bucket
+     *
      * @return Boolean
      */
     public Boolean removeBucket(String bucketName) {
         try {
-            minioClient.removeBucket(RemoveBucketArgs.builder()
-                    .bucket(bucketName)
-                    .build());
+            minioClient.removeBucket(RemoveBucketArgs.builder().bucket(bucketName).build());
         } catch (Exception e) {
             e.printStackTrace();
             return false;
         }
         return true;
     }
+
     /**
      * 获取全部bucket
      */
@@ -96,7 +97,6 @@ public class MinioUtil {
 //        }
 //        return null;
 //    }
-
     public List<String> getAllBuckets() {
         List<String> bucketNames = new ArrayList<>();
         try {
@@ -120,9 +120,7 @@ public class MinioUtil {
 
     private void listFilesRecursive(String bucketName, String prefix, List<String> fileNames) {
         try {
-            Iterable<Result<Item>> results = minioClient.listObjects(
-                    ListObjectsArgs.builder().bucket(bucketName).prefix(prefix).recursive(false).build()
-            );
+            Iterable<Result<Item>> results = minioClient.listObjects(ListObjectsArgs.builder().bucket(bucketName).prefix(prefix).recursive(false).build());
             for (Result<Item> result : results) {
                 Item item = result.get();
                 if (item.isDir()) {
@@ -146,14 +144,13 @@ public class MinioUtil {
      */
     public String upload(MultipartFile file) {
         String originalFilename = file.getOriginalFilename();
-        if (StringUtils.isBlank(originalFilename)){
+        if (StringUtils.isBlank(originalFilename)) {
             throw new RuntimeException();
         }
         String fileName = generateUuid() + originalFilename.substring(originalFilename.lastIndexOf("."));
         String objectName = getNowDateLongStr("yyyy-MM/dd") + "/" + fileName;
         try {
-            PutObjectArgs objectArgs = PutObjectArgs.builder().bucket(prop.getBucketName()).object(objectName)
-                    .stream(file.getInputStream(), file.getSize(), -1).contentType(file.getContentType()).build();
+            PutObjectArgs objectArgs = PutObjectArgs.builder().bucket(prop.getBucketName()).object(objectName).stream(file.getInputStream(), file.getSize(), -1).contentType(file.getContentType()).build();
             //文件名称相同会覆盖
             minioClient.putObject(objectArgs);
         } catch (Exception e) {
@@ -165,10 +162,11 @@ public class MinioUtil {
 
     /**
      * 预览图片
+     *
      * @param fileName
      * @return
      */
-    public String preview(String fileName){
+    public String preview(String fileName) {
         // 查看文件地址
         GetPresignedObjectUrlArgs build = new GetPresignedObjectUrlArgs().builder().bucket(prop.getBucketName()).object(fileName).method(Method.GET).build();
         try {
@@ -182,19 +180,19 @@ public class MinioUtil {
 
     /**
      * 文件下载
+     *
      * @param fileName 文件名称
-     * @param res response
+     * @param res      response
      * @return Boolean
      */
     public void download(String fileName, HttpServletResponse res) {
-        GetObjectArgs objectArgs = GetObjectArgs.builder().bucket(prop.getBucketName())
-                .object(fileName).build();
-        try (GetObjectResponse response = minioClient.getObject(objectArgs)){
+        GetObjectArgs objectArgs = GetObjectArgs.builder().bucket(prop.getBucketName()).object(fileName).build();
+        try (GetObjectResponse response = minioClient.getObject(objectArgs)) {
             byte[] buf = new byte[1024];
             int len;
-            try (FastByteArrayOutputStream os = new FastByteArrayOutputStream()){
-                while ((len=response.read(buf))!=-1){
-                    os.write(buf,0,len);
+            try (FastByteArrayOutputStream os = new FastByteArrayOutputStream()) {
+                while ((len = response.read(buf)) != -1) {
+                    os.write(buf, 0, len);
                 }
                 os.flush();
                 byte[] bytes = os.toByteArray();
@@ -202,7 +200,7 @@ public class MinioUtil {
                 // 设置强制下载不打开
                 // res.setContentType("application/force-download");
                 res.addHeader("Content-Disposition", "attachment;fileName=" + fileName);
-                try (ServletOutputStream stream = res.getOutputStream()){
+                try (ServletOutputStream stream = res.getOutputStream()) {
                     stream.write(bytes);
                     stream.flush();
                 }
@@ -214,11 +212,11 @@ public class MinioUtil {
 
     /**
      * 查看文件对象
+     *
      * @return 存储bucket内文件对象信息
      */
     public List<Item> listObjects() {
-        Iterable<Result<Item>> results = minioClient.listObjects(
-                ListObjectsArgs.builder().bucket(prop.getBucketName()).build());
+        Iterable<Result<Item>> results = minioClient.listObjects(ListObjectsArgs.builder().bucket(prop.getBucketName()).build());
         List<Item> items = new ArrayList<>();
         try {
             for (Result<Item> result : results) {
@@ -233,14 +231,15 @@ public class MinioUtil {
 
     /**
      * 删除
+     *
      * @param fileName
      * @return
      * @throws Exception
      */
-    public boolean remove(String fileName){
+    public boolean remove(String fileName) {
         try {
-            minioClient.removeObject( RemoveObjectArgs.builder().bucket(prop.getBucketName()).object(fileName).build());
-        }catch (Exception e){
+            minioClient.removeObject(RemoveObjectArgs.builder().bucket(prop.getBucketName()).object(fileName).build());
+        } catch (Exception e) {
             return false;
         }
         return true;
