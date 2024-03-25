3cbb09bce773c2bd8268a48377f46fdd9f628402
Wenhao <yuwenhao@freezonex.io>
2024-03-25 11:16:42 +0800

    新增

---
+++ src/main/java/com/supos/app/config/JacksonConfiguration.java
@@ -0,0 +1,17 @@
+package com.supos.app.config;
+
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfiguration {
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
+        return builder -> {
+            // Long 会自定转换成 String
+            builder.serializerByType(Long.class, ToStringSerializer.instance);
+        };
+    }
+}
