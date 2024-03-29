e374843ab2a57ff9f2fd521d7e8113b7c8918f5f
Wenhao <yuwenhao@freezonex.io>
2024-03-29 23:22:45 +0800

    新增

--- src/main/java/com/supos/app/controller/WmsFrontend.java
+++ src/main/java/com/supos/app/controller/WmsFrontend.java
@@ -22,7 +22,7 @@ public class WmsFrontend {
             @ApiResponse(code = 200, message = "OK")
             // Do not add other status codes if you do not want them documented
     })
-    @GetMapping("/apps/wenhao-javaw/analysis/3d")
+    @GetMapping("/analysis/3d")
     public String view30() {
         return "forward:/apps/wenhao-javaw/analysis/3d.html";
     }
@@ -30,7 +30,7 @@ public class WmsFrontend {
     @ApiOperation(value = "Material Inbound",
             notes = "Returns an HTML page for Material Inbound. " +
             "\n\nResponse details are not elaborated since the response is HTML content.")
-    @GetMapping("/apps/wenhao-javaw/operation/inbound")
+    @GetMapping("/operation/inbound")
     public String view31() {
         return "forward:/apps/wenhao-javaw/operation/inbound.html";
     }
@@ -38,7 +38,7 @@ public class WmsFrontend {
     @ApiOperation(value = "Material Inbound Create",
             notes = "Returns an HTML page for Material Inbound Create. " +
             "\n\nResponse details are not elaborated since the response is HTML content.")
-    @GetMapping("/apps/wenhao-javaw/operation/inbound/create")
+    @GetMapping("/operation/inbound/create")
     public String view32() {
         return "forward:/apps/wenhao-javaw/operation/inbound/create.html";
     }
@@ -46,14 +46,14 @@ public class WmsFrontend {
     @ApiOperation(value = "Material Outbound",
             notes = "Returns an HTML page for Material Inbound. " +
                     "\n\nResponse details are not elaborated since the response is HTML content.")
-    @GetMapping("/apps/wenhao-javaw/operation/outbound")
+    @GetMapping("/operation/outbound")
     public String view33() {
         return "forward:/apps/wenhao-javaw/operation/outbound.html";
     }
     @ApiOperation(value = "Material Outbound Create",
             notes = "Returns an HTML page for Material Inbound. " +
                     "\n\nResponse details are not elaborated since the response is HTML content.")
-    @GetMapping("/apps/wenhao-javaw/operation/outbound/create")
+    @GetMapping("/operation/outbound/create")
     public String view34() {
         return "forward:/apps/wenhao-javaw/operation/outbound/create.html";
     }
@@ -62,7 +62,7 @@ public class WmsFrontend {
             notes = "Returns an HTML page for Material Display. " +
                     "\n\nDisplay material information. " +
                     "\n\nResponse details are not elaborated since the response is HTML content.")
-    @GetMapping("/apps/wenhao-javaw/warehouse/material")
+    @GetMapping("/warehouse/material")
     public String view35() {
         return "forward:/apps/wenhao-javaw/warehouse/material.html";
     }
@@ -70,7 +70,7 @@ public class WmsFrontend {
     @ApiOperation(value = "Material Create",notes = "Returns an HTML page for Material Create." +
             " \n\nCreating a new material. " +
             "\n\nResponse details are not elaborated since the response is HTML content.")
-    @GetMapping("/apps/wenhao-javaw/warehouse/material/create")
+    @GetMapping("/warehouse/material/create")
     public String view36() {
         return "forward:/apps/wenhao-javaw/warehouse/material/create.html";
     }
@@ -78,7 +78,7 @@ public class WmsFrontend {
     @ApiOperation(value = "Rfid Material",notes = "Returns an HTML page for Rfid Material. " +
             "\n\nDisplay the rfid of  materials. " +
             "\n\nResponse details are not elaborated since the response is HTML content.")
-    @GetMapping("/apps/wenhao-javaw/warehouse/material/rfid")
+    @GetMapping("/warehouse/material/rfid")
     public String view37() {
         return "forward:/apps/wenhao-javaw/warehouse/material/rfid.html";
     }
@@ -86,7 +86,7 @@ public class WmsFrontend {
     @ApiOperation(value = "Rfid Material Create",notes = "Returns an HTML page for Rfid Material Crreate. " +
             "\n\nUsing Rfid create a material. " +
             "\n\nResponse details are not elaborated since the response is HTML content.")
-    @GetMapping("/apps/wenhao-javaw/warehouse/material/rfid/create")
+    @GetMapping("/warehouse/material/rfid/create")
     public String view38() {
         return "forward:/apps/wenhao-javaw/warehouse/material/rfid/create.html";
     }
@@ -94,14 +94,14 @@ public class WmsFrontend {
     @ApiOperation(value = "Warehouse",notes = "Returns an HTML page for warehouse. " +
             "\n\nDisplay warehouse information. " +
             "\n\nResponse details are not elaborated since the response is HTML content.")
-    @GetMapping("/apps/wenhao-javaw/warehouse")
+    @GetMapping("/warehouse")
     public String view39() {
         return "forward:/apps/wenhao-javaw/warehouse.html";
     }
     @ApiOperation(value = "Login",notes = "Returns an HTML page for **Login**. " +
             "\n\nLogin page ." +
             "\n\nResponse details are not elaborated since the response is HTML content.")
-    @GetMapping("/apps/wenhao-javaw/login")
+    @GetMapping("/login")
     public String view40() {
         return "forward:/apps/wenhao-javaw/login.html";
     }
@@ -113,7 +113,7 @@ public class WmsFrontend {
             "- Top side record information \n" +
             "- middle side supplier information  \n" +
             "\n" )
-    @GetMapping("/apps/wenhao-javaw/analysis")
+    @GetMapping("/analysis")
     public String view41() {
         return "forward:/apps/wenhao-javaw/analysis.html";
     }
@@ -124,7 +124,7 @@ public class WmsFrontend {
     @ApiResponses(value = {
             @ApiResponse(code = 404, message = "404 not found",response = SuposApi.class)
     })
-    @GetMapping("/apps/wenhao-javaw/404")
+    @GetMapping("/404")
     public String view42() {
         return "forward:/apps/wenhao-javaw/404.html";
     }
@@ -132,7 +132,7 @@ public class WmsFrontend {
     @ApiOperation(value = "Stocktaking",notes = "Returns an HTML page for Stocktaking. " +
             "\n\nMaterial inventory information display. " +
             "\n\nResponse details are not elaborated since the response is HTML content.")
-    @GetMapping("/apps/wenhao-javaw/operation/stocktaking")
+    @GetMapping("/operation/stocktaking")
     public String view43() {
         return "forward:/apps/wenhao-javaw/operation/stocktaking.html";
     }
@@ -140,7 +140,7 @@ public class WmsFrontend {
     @ApiOperation(value = "Stocktaking Create",notes = "Returns an HTML page for Stocktaking Create. " +
             "\n\nCreating new stocktaking task. " +
             "\n\nResponse details are not elaborated since the response is HTML content.")
-    @GetMapping("/apps/wenhao-javaw/operation/stocktaking/create")
+    @GetMapping("/operation/stocktaking/create")
     public String view44() {
         return "forward:/apps/wenhao-javaw/operation/stocktaking/create.html";
     }
@@ -148,7 +148,7 @@ public class WmsFrontend {
     @ApiOperation(value = "Index",notes = "Returns an HTML page for index. " +
             "\n\nIndex of all the pages. " +
             "\n\nResponse details are not elaborated since the response is HTML content.")
-    @GetMapping("/apps/wenhao-javaw/index")
+    @GetMapping("/index")
     public String view45() {
         return "forward:/apps/wenhao-javaw/index.html";
     }
@@ -156,7 +156,7 @@ public class WmsFrontend {
     @ApiOperation(value = "Home",notes = "Returns an HTML page for home. " +
             "\n\nHome pages. " +
             "\n\nResponse details are not elaborated since the response is HTML content.")
-    @GetMapping("/apps/wenhao-javaw/home")
+    @GetMapping("/home")
     public String view46() {
         return "forward:/apps/wenhao-javaw/home.html";
     }
